e6439ac93972a98d7ba6d59de07aaae0c71b91e0
Alex Wilmer <djazium@gmail.com>
2023-08-02 18:06:05 -0400

    port env

--- api/main.js
+++ api/main.js
@@ -2,10 +2,12 @@ let express = require("express");
 
 let app = express();
 
+let PORT = process.env.PORT;
+
 app.get("/", (req, res) => {
   res.json({ messge: "hello world " });
 });
 
-app.listen(8080, () => {
-  console.log("app running on 8080");
+app.listen(PORT, () => {
+  console.log(`app running on ${PORT}`);
 });
